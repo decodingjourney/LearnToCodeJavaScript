f97cf291e890fec8c6ce179873c1702791df4638
anandkumarjhaic <37099338+anandkumarjhaic@users.noreply.github.com>
2018-09-27 17:34:00 +0530

    updated code of Challenges

--- JavaScript/5-advanced-JS/starter/script.js
+++ JavaScript/5-advanced-JS/starter/script.js
@@ -249,7 +249,8 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
-function Question(question, answers, correct) {
+/*(function() {
+    function Question(question, answers, correct) {
     this.question = question;
     this.answers = answers;
     this.correct = correct;
@@ -263,6 +264,14 @@ Question.prototype.displayQandA = function() {
     }
 }
 
+Question.prototype.checkAnswer = function(ans) {
+    if(ans === this.correct) {
+        console.log('congratulations! it\'s a right answer');
+    }else {
+        console.log('Wrong answer! Please try again');
+    }
+}
+
 var q1 = new Question('is JavaScript the coolest language?', ['yes' ,'no'], 0);
 var q2 = new Question('Are you mad about coding?', ['yes' , 'no'], 0);
 var q3 = new Question('Do you watch hollywood movies?', ['yes' , 'no'], 0);
@@ -273,10 +282,69 @@ var n = Math.floor(Math.random() * questions.length);
 
 questions[n].displayQandA();
 
+var answer = parseInt(prompt('Please answer the question correctly!'));
+
+
+questions[n].checkAnswer(answer);
+})();*/
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
 
 
 
+(function() {
+    function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+}
 
+Question.prototype.displayQandA = function() {
+    console.log(this.question);
+    
+    for(var i = 0; i < this.answers.length; i++) {
+        console.log(i+': '+this.answers[i]);
+    }
+}
+
+Question.prototype.checkAnswer = function(ans) { 
+    if(ans === this.correct) {
+        console.log('congratulations! it\'s a right answer');
+    }else {
+        console.log('Wrong answer! Please try again');
+    }
+}
+
+var q1 = new Question('is JavaScript the coolest language?', ['yes' ,'no'], 0);
+var q2 = new Question('Are you mad about coding?', ['yes' , 'no'], 0);
+var q3 = new Question('Do you watch hollywood movies?', ['yes' , 'no'], 0);
+
+var questions = [q1,q2,q3];
+    function nextQeustion() {
+        var n = Math.floor(Math.random() * questions.length);
+
+questions[n].displayQandA();
+
+var answer = prompt('Please answer the question correctly!');
+
+if(answer != 'exit') {
+    questions[n].checkAnswer(parseInt(answer));
+    nextQeustion();
+}
+
+    }
+    nextQeustion();
+})();
 
 
 
